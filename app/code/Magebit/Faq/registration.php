dfbb55b756bf9b977f692e2a44c9d55cc0543c21
Vitalijs Mikutis <vitalijs.mikutis@magebit.com>
2024-09-27 13:09:25 +0300

    first commit for MLO2

--- app/code/Magebit/Faq/registration.php
+++ app/code/Magebit/Faq/registration.php
@@ -1,5 +1,10 @@
 <?php
-\Magento\Framework\Component\ComponentRegistrar::register(
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Magebit_Faq',
     __DIR__
